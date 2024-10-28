f1c7856ccc6e8f35c5cbf4e26b8970473f307a3e
Marc White <mwhite1206@gmail.com>
2024-10-28 16:08:19 +1100

    WIP on data.utils

---
+++ src/access_nri_intake/data/utils.py
@@ -0,0 +1,46 @@
+# Copyright 2024 ACCESS-NRI and contributors. See the top-level COPYRIGHT file for details.
+# SPDX-License-Identifier: Apache-2.0
+
+import re
+
+import yaml
+
+from ..utils import get_catalog_fp
+
+CATALOG_PATH_REGEX = r"^(?P<rootpath>.*?)\{\{version\}\}.*?$"
+
+
+def _get_catalog_rp():
+    """
+    Get the catalog root path.
+    """
+    with open(get_catalog_fp()) as fo:
+        catalog_metadata = yaml.load(fo, yaml.FullLoader)
+
+    try:
+        catalog_fp = catalog_metadata["sources"]["access_nri"]["args"]["path"]
+    except KeyError:
+        raise RuntimeError(
+            f"Catalog metadata {get_catalog_fp()} does not match expected format."
+        )
+
+    match = re.match(CATALOG_PATH_REGEX, catalog_fp)
+    try:
+        return match.group("rootpath")
+    except AttributeError:  # Match failed
+        raise RuntimeError(
+            f"Catalog metadata {get_catalog_fp()} contains unexpected catalog filepath: {catalog_fp}"
+        )
+
+
+def available_versions(pretty: bool = True):
+    """
+    Report the available versions of the `intake.cat.access_nri` catalog.
+
+    Parameters
+    ---------
+    pretty : bool, optional
+        Defines whether to return a pretty print-out of the available versions
+        (True, default), or to provide a list of version numbers only (False).
+    """
+    pass
