900fae2e9de71d4c933d233282acf29f64151ec8
Marc White <mwhite1206@gmail.com>
2024-10-28 16:00:32 +1100

    Give RuntimeWarning, not error, if live catalog can't be linked

--- src/access_nri_intake/data/__init__.py
+++ src/access_nri_intake/data/__init__.py
@@ -1,8 +1,16 @@
 # Copyright 2023 ACCESS-NRI and contributors. See the top-level COPYRIGHT file for details.
 # SPDX-License-Identifier: Apache-2.0
 
+import warnings
+
 import intake
 
 from access_nri_intake.utils import get_catalog_fp
 
-data = intake.open_catalog(get_catalog_fp()).access_nri
+try:
+    data = intake.open_catalog(get_catalog_fp()).access_nri
+except FileNotFoundError:
+    warnings.warn(
+        "Unable to load catalog. Calling intake.cat.access_nri will not work.",
+        RuntimeWarning,
+    )
