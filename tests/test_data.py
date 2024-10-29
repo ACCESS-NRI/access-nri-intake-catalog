1ec48c1f8b2d0e3bdb478a189bf7df2f7870a535
Marc White <mwhite1206@gmail.com>
2024-10-29 11:18:40 +1100

    First test for _get_catalog_rp

--- tests/test_data.py
+++ tests/test_data.py
@@ -3,8 +3,10 @@
 
 import os
 from pathlib import Path
+from unittest import mock
 
 import access_nri_intake
+from access_nri_intake.data.utils import _get_catalog_rp
 from access_nri_intake.utils import get_catalog_fp
 
 
@@ -19,3 +21,20 @@ def test_get_catalog_fp():
     catalog_fullpath = get_catalog_fp()
 
     assert str(catalog_fullpath) == expected_path
+
+
+@mock.patch("access_nri_intake.data.utils.get_catalog_fp")
+def test__get_catalog_rp(mock_get_catalog_fp, test_data):
+    """
+    Check that we correctly decipher to rootpath (rp) to the catalogs
+    """
+    mock_get_catalog_fp.return_value = test_data / "catalog/catalog-good.yaml"
+    assert (
+        access_nri_intake.data.utils.get_catalog_fp()
+        == test_data / "catalog/catalog-good.yaml"
+    ), "Mock failed"
+
+    rp = _get_catalog_rp()
+    assert (
+        rp == "/this/is/root/path/"
+    ), f"Computed root path {rp} != expected value /this/is/root/path/"
