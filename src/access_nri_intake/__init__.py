a375b7b08cb7d16c249e372c11863fb55bfc9f92
Marc White <mwhite1206@gmail.com>
2024-12-09 14:01:25 +1100

    Revert "WIP, commit for safekeeping"
    This reverts commit 1ce9631cfb70fb07483cb9aaafcae65ba60383e3.

--- src/access_nri_intake/__init__.py
+++ src/access_nri_intake/__init__.py
@@ -7,8 +7,5 @@ from . import _version
 
 __version__ = _version.get_versions()["version"]
 
-
 CATALOG_LOCATION = "/g/data/xp65/public/apps/access-nri-intake-catalog/catalog.yaml"
 USER_CATALOG_LOCATION = Path.home() / ".access_nri_intake_catalog/catalog.yaml"
-
-CATALOG_VERSION_REGEX = "v(?P<year>[0-9]{4})"
