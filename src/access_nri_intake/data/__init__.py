20e958ee14ea6b0dc3a19ec8d7120ecc7c90006b
Charles Turner <charles.turner@anu.edu.au>
2024-12-23 12:56:30 +0800

    Move telemetry out to separate package

--- src/access_nri_intake/data/__init__.py
+++ src/access_nri_intake/data/__init__.py
@@ -5,35 +5,25 @@ import warnings
 
 import intake
 import intake.catalog
-from IPython import get_ipython
-from IPython.core.magic import register_line_magic
+from access_ipy_telemetry.cli import configure_telemetry
+from access_ipy_telemetry.utils import ApiHandler
 
 from access_nri_intake.utils import get_catalog_fp
 
-from .telemetry import capture_datastore_searches
+api_handler = ApiHandler()
 
 CATALOG_NAME_FORMAT = (
     r"^v(?P<yr>2[0-9]{3})\-(?P<mon>1[0-2]|0[1-9])\-(?P<day>0[1-9]|[1-2][0-9]|3[0-1])$"
 )
 
 
-def load_ipython_extension(ipython):
-    @register_line_magic("capture_func_calls")
-    def capture_func_calls(info):
-        """
-        Returns the function calls from the code in the cell
-        """
-        ipython.events.register("pre_run_cell", capture_datastore_searches)
-
-
-# Register the extension
-ip = get_ipython()
-if ip:
-    load_ipython_extension(ip)
-    ip.run_line_magic("capture_func_calls", "")
-
 try:
     data = intake.open_catalog(get_catalog_fp()).access_nri
+    cat_version = data._captured_init_kwargs.get("metadata", {}).get(
+        "version", "latest"
+    )  # Get the catalog version number and set it to "latest" if it can't be found
+    configure_telemetry(["--enable", "--silent"])
+    api_handler.add_extra_field("catalog", {"catalog_version": cat_version})
 except FileNotFoundError:
     warnings.warn(
         "Unable to access a default catalog location. Calling intake.cat.access_nri will not work.",
