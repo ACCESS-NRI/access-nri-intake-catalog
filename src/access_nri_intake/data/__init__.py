9643c178467fb7cdb35c7dbfb201ecc01f694ab6
Charles Turner <charles.turner@anu.edu.au>
2025-01-22 11:10:40 +0800

    Update URL to point to dev server, fix dependency name in pyproject.toml

--- src/access_nri_intake/data/__init__.py
+++ src/access_nri_intake/data/__init__.py
@@ -5,12 +5,13 @@ import warnings
 
 import intake
 import intake.catalog
-from access_ipy_telemetry.api import ApiHandler
-from access_ipy_telemetry.cli import configure_telemetry
+from access_py_telemetry.api import ApiHandler
+from access_py_telemetry.cli import configure_telemetry
 
 from access_nri_intake.utils import get_catalog_fp
 
 api_handler = ApiHandler()
+api_handler.server_url = "https://reporting-dev.access-nri-store.cloud.edu.au"
 
 CATALOG_NAME_FORMAT = (
     r"^v(?P<yr>2[0-9]{3})\-(?P<mon>1[0-2]|0[1-9])\-(?P<day>0[1-9]|[1-2][0-9]|3[0-1])$"
@@ -22,8 +23,6 @@ try:
     cat_version = data._captured_init_kwargs.get("metadata", {}).get(
         "version", "latest"
     )  # Get the catalog version number and set it to "latest" if it can't be found
-    configure_telemetry(["--enable", "--silent"])
-    api_handler.add_extra_field("catalog", {"catalog_version": cat_version})
 except FileNotFoundError:
     warnings.warn(
         "Unable to access a default catalog location. Calling intake.cat.access_nri will not work.",
@@ -31,3 +30,6 @@ except FileNotFoundError:
         stacklevel=2,
     )
     data = intake.catalog.Catalog()
+finally:
+    configure_telemetry(["--enable", "--silent"])
+    api_handler.add_extra_field("catalog", {"catalog_version": cat_version})
