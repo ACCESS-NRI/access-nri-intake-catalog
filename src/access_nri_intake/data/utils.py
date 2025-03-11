7410341db0e1578f25726849138956894d50d550
Marc White <mwhite1206@gmail.com>
2025-03-11 12:49:03 +1100

    Fix bug in parent call

--- src/access_nri_intake/data/utils.py
+++ src/access_nri_intake/data/utils.py
@@ -98,7 +98,7 @@ def available_versions(pretty: bool = True) -> list[str] | None:
         # In pretty mode, we want to look for & return the catalogs that are referred to
         # by outdated catalog files (catalog-YYYYMMDD-YYYYMMDD)
         # Locate the outdated catalog files
-        catalog_loc = get_catalog_fp().parent()
+        catalog_loc = Path(get_catalog_fp())
         old_cats = catalog_loc.glob("catalog-*-*.yaml")
 
         for cat in old_cats:
