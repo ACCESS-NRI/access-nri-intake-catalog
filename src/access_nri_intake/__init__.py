d6d5e87c5b2dfc83d87e824924c4c4b719d4f05e
Marc White <mwhite1206@gmail.com>
2025-03-13 13:29:42 +1100

    Document storage & location variables

--- src/access_nri_intake/__init__.py
+++ src/access_nri_intake/__init__.py
@@ -8,8 +8,16 @@ from . import _version
 __version__ = _version.get_versions()["version"]
 
 CATALOG_LOCATION = "/g/data/xp65/public/apps/access-nri-intake-catalog/catalog.yaml"
-USER_CATALOG_LOCATION = str(Path.home() / ".access_nri_intake_catalog/catalog.yaml")
+"""Location for 'live' master catalog YAML."""
 
-STORAGE_FLAG_PATTERN = "gdata/[a-z]{1,2}[0-9]{1,2}"
-STORAGE_LOCATION_REGEX = r"^/g/data/(?P<proj>[a-z]{1,2}[0-9]{1,2})/.*?$"
+USER_CATALOG_LOCATION = str(Path.home() / ".access_nri_intake_catalog/catalog.yaml")
+"""Location where user can place a master catalog YAML to override standard 'live' version."""
+
 STORAGE_ROOT = "/g/data"
+"""Root storage location for catalog experiments"""
+
+STORAGE_FLAG_PATTERN = r"gdata/[a-z]{1,2}[0-9]{1,2}"
+"""Pattern for matching 'storage flags' - related to Gadi file access system"""
+
+STORAGE_LOCATION_REGEX = r"^/g/data/(?P<proj>[a-z]{1,2}[0-9]{1,2})/.*?$"
+"""Regular expression for matching the file path to experiments, and extracting a project ID"""
