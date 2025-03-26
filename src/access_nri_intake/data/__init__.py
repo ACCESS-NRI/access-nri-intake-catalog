8289b6be1fd6aa54f40d0e138e0ba397ee4c17ec
Charles Turner <charles.turner@anu.edu.au>
2025-03-26 13:43:55 +0800

    Update telemetry setup for some testing

--- src/access_nri_intake/data/__init__.py
+++ src/access_nri_intake/data/__init__.py
@@ -11,7 +11,7 @@ from access_py_telemetry.cli import configure_telemetry
 from access_nri_intake.utils import get_catalog_fp
 
 api_handler = ApiHandler()
-api_handler.server_url = "https://reporting-dev.access-nri-store.cloud.edu.au"
+api_handler.server_url = "https://reporting-dev.access-nri-store.cloud.edu.au/api"
 
 CATALOG_NAME_FORMAT = (
     r"^v(?P<yr>2[0-9]{3})\-(?P<mon>1[0-2]|0[1-9])\-(?P<day>0[1-9]|[1-2][0-9]|3[0-1])$"
