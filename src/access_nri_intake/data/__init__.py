37429ec0ae62fcecc57e3922adc5228ef69e53c7
Charles Turner <charles.turner@anu.edu.au>
2025-04-01 11:29:56 +0800

    Update to point at production server

--- src/access_nri_intake/data/__init__.py
+++ src/access_nri_intake/data/__init__.py
@@ -6,20 +6,21 @@ from pathlib import Path
 
 import intake
 import intake.catalog
-from access_py_telemetry.api import ApiHandler
+from access_py_telemetry.api import ApiHandler, ProductionToggle
 from access_py_telemetry.cli import configure_telemetry
 
 from access_nri_intake.utils import get_catalog_fp
 
 token = (
-    Path("/g/data/xp65/admin/access-nri-intake-catalog/TELEMETRY_TOKEN")
+    Path("/g/data/xp65/admin/access-nri-intake-catalog/TELEMETRY_TOKEN_PRODUCTION")
     .read_text(encoding="utf8")
     .strip()
 )
 
 api_handler = ApiHandler()
-api_handler.server_url = "https://reporting-dev.access-nri-store.cloud.edu.au/api"
 api_handler.set_headers(None, {"Authorization": f"Token {token}"})
+
+ProductionToggle().production = True
 
 CATALOG_NAME_FORMAT = (
     r"^v(?P<yr>2[0-9]{3})\-(?P<mon>1[0-2]|0[1-9])\-(?P<day>0[1-9]|[1-2][0-9]|3[0-1])$"
