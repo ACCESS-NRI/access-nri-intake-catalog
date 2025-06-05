fa2c5b5d6cd4ab41ba7037acb2d2dee705d46cbd
Charles Turner <charles.turner@anu.edu.au>
2025-06-05 15:49:55 +0800

    Update help message to be actually helpful

--- src/access_nri_intake/cli.py
+++ src/access_nri_intake/cli.py
@@ -448,7 +448,7 @@ def build(argv: Sequence[str] | None = None):
         default=False,
         action="store_true",
         help=(
-            "Set this if you don't want to update the access_nri_intake.data (e.g. if running a test)"
+            "Set this if you don't want to concretize the build, ie. keep it .version & not update catalog.yaml"
         ),
     )
 
