ec7c28b04f938ccce2ad3b462f32ab0af7230fb2
Charles Turner <charles.turner@anu.edu.au>
2025-06-18 11:56:24 +0800

    Add WoaBuilder test skeleton for test_builder_build

--- tests/test_builders.py
+++ tests/test_builders.py
@@ -32,6 +32,7 @@ from access_nri_intake.source.utils import _NCFileInfo
         (["access-om3"], "AccessOm3Builder", {}, 12, 12, 6),
         (["mom6"], "Mom6Builder", {}, 27, 27, 15),
         (["roms"], "ROMSBuilder", {}, 4, 4, 1),
+        (["woa"], "WOABuilder", {}, 2, 2, 1),
     ],
 )
 def test_builder_build(
