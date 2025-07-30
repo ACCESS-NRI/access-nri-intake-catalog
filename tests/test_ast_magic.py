42d75266c73d75a84d44114b79a1028e56716ae7
Charles Turner <charles.turner@anu.edu.au>
2025-07-30 15:11:04 +0800

    It's TDD time

--- tests/test_ast_magic.py
+++ tests/test_ast_magic.py
@@ -111,3 +111,25 @@ undefined_datastore.{func}()
     assert ipython.find_magic("check_storage_enabled", "cell") is not None
 
     check_storage_enabled("", raw_cell)
+
+
+@pytest.mark.parametrize(
+    "func",
+    [
+        "to_dataset_dict",
+        "to_dask",
+        "to_datatree",
+    ],
+)
+def test_chained_to_datasets_raises(ipython, test_data, func):
+    raw_cell = f"""
+%%check_storage_enabled
+import intake
+ds = intake.open_esm_datastore("{test_data}/esm_datastore/cmip-forcing-qv56.json")
+ds.search(source_id='.*').{func}()
+"""
+
+    assert ipython.find_magic("check_storage_enabled", "cell") is not None
+
+    with pytest.raises(MissingStorageError):
+        check_storage_enabled("", raw_cell)
