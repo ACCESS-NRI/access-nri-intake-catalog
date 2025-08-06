dce3e3126be1a4a539106c4a1d4976e5c17a3093
Charles Turner <charles.turner@anu.edu.au>
2025-08-06 09:26:13 +0800

    Add ipython magic to check the number of datasets for a .to_dask() call

--- src/access_nri_intake/ipython_magic/__init__.py
+++ src/access_nri_intake/ipython_magic/__init__.py
@@ -2,12 +2,16 @@ from .ast import (
     CallListener,
     MissingStorageError,
     MissingStorageWarning,
+    TooManyDatasetsError,
+    check_dataset_number,
     check_storage_enabled,
 )
 
 __all__ = [
     "CallListener",
     "check_storage_enabled",
+    "TooManyDatasetsError",
+    "check_dataset_number",
     "MissingStorageError",
     "MissingStorageWarning",
 ]
--- src/access_nri_intake/ipython_magic/ast.py
+++ src/access_nri_intake/ipython_magic/ast.py
@@ -5,10 +5,9 @@
 
 import re
 import warnings
-from typing import Any
+from typing import Any, Callable
 
 import libcst as cst
-import libcst.matchers as m
 from IPython.core.getipython import get_ipython
 from IPython.core.interactiveshell import InteractiveShell
 from IPython.core.magic import cell_magic
@@ -29,12 +28,24 @@ class MissingStorageError(Exception):
     pass
 
 
-def check_permissions(esm_datastore, err: Exception | None = None) -> None:
+class TooManyDatasetsError(Exception):
+    """Error raised when we try to call .to_dask() but have >1 dataset"""
+
+    pass
+
+
+def check_permissions(
+    esm_datastore, method_name: str, err: Exception | None = None
+) -> None:
     """
     Use an IPython cell magic to listen for calls to `.to_dask`, `.to_dataset_dict()` or `to_datatree`, and
     inspect the list of paths attached to the associated esm_datastore. If we find any paths that we don't
     have the relevant permissions for, then emit a warning
     """
+
+    if method_name not in ["to_dask", "to_dataset_dict", "to_datatree"]:
+        return None
+
     project_codes = set(esm_datastore.df["path"].map(_get_project_code))
 
     access_valid, error_msg = _confirm_project_access(project_codes)
@@ -50,6 +61,36 @@ def check_permissions(esm_datastore, err: Exception | None = None) -> None:
             # `check_load_calls` magic. So we just raise a new error
             raise MissingStorageError(error_msg)
 
+    return None
+
+
+def check_multiple_datasets(
+    esm_datastore, method_name: str, err: Exception | None = None
+) -> None:
+    """
+    Use an IPython cell magic to listen for calls to `.to_dask`, `.to_dataset_dict()` or `to_datatree`, and
+    inspect the list of datasets attached to the associated esm_datastore. If we find more than one dataset,
+    then raise an error.
+    """
+    if method_name != "to_dask":
+        return None
+
+    if len(esm_datastore) > 1:
+        groupby_attrs = esm_datastore.esmcat.aggregation_control.groupby_attrs
+        uniq = esm_datastore.unique()
+        too_long = []
+        for attr in groupby_attrs:
+            if len(uniq[attr]) > 1:
+                too_long.append((attr, len(uniq[attr])))
+
+        if too_long:
+            err_msg = (
+                f"Found >1 dataset: distinguished on {[f'{(attr[0])} ({attr[1]} values), ' for attr in too_long]}."
+                " Please refine search further, use `.to_dataset_dict()`/`.to_datatree, or change"
+                " aggregation controls: see https://github.com/COSIMA/cosima-recipes/issues/543#issuecomment-3086429836"
+            )
+
+            raise TooManyDatasetsError(err_msg)
     return None
 
 
@@ -116,17 +157,67 @@ def check_storage_enabled(line, cell) -> None:
 
     reducer = ChainSimplifier(user_namespace)
     reduced_tree = tree.visit(reducer)
-    visitor = CallListener(user_namespace, _err)
+    visitor = CallListener(user_namespace, check_permissions, _err)
+    reduced_tree.visit(visitor)
+
+    return None
+
+
+@cell_magic
+def check_dataset_number(line, cell) -> None:
+    """
+    Use the AST module to parse the code that we are executing & check for attempts
+    to open multiple datasets with `.to_dask()`, so we can inform the user why
+
+    Fail silently if we can't parse the code.
+
+    Parameters
+    ----------
+    line:   str
+        The line of the cell magic (not used here, but required by IPython).
+
+    cell : str
+        The code to parse.
+
+    Returns
+    -------
+    None
+    """
+    code = cell
+
+    code = strip_magic(code)
+
+    try:
+        tree = cst.parse_module(code)
+    except (SyntaxError, ParserSyntaxError, IndentationError):
+        return None
+
+    ip = get_ipython()  # type: ignore
+
+    result = ip.run_cell(cell)
+    _err = result.error_in_exec if result.error_in_exec else None
+
+    user_namespace: dict[str, Any] = get_ipython().user_ns  # type: ignore
+
+    reducer = ChainSimplifier(user_namespace)
+    reduced_tree = tree.visit(reducer)
+    visitor = CallListener(user_namespace, check_multiple_datasets, _err)
     reduced_tree.visit(visitor)
 
     return None
 
 
 class CallListener(cst.CSTVisitor):
-    def __init__(self, user_namespace: dict[str, Any], _err: Exception | None = None):
+    def __init__(
+        self,
+        user_namespace: dict[str, Any],
+        check_func: Callable,
+        _err: Exception | None = None,
+    ):
         self.user_namespace = user_namespace
         self._caught_calls: set[str] = set()  # Mostly for debugging
         self._err = _err
+        self.check_func = check_func
 
     def visit_Call(self, node: cst.Call) -> None:
         """
@@ -146,18 +237,8 @@ class CallListener(cst.CSTVisitor):
 
             # Check if it's an esm_datastore with a load method
             class_name = type(instance).__name__
-            if class_name == "esm_datastore" and self._is_load_call(method_name):
-                check_permissions(instance, self._err)
-
-    def _is_load_call(self, func_name: str) -> bool:
-        """
-        Check if the function name is one of the load calls we are interested in.
-        """
-        return (
-            func_name.endswith("to_dask")
-            or func_name.endswith("to_dataset_dict")
-            or func_name.endswith("to_datatree")
-        )
+            if class_name == "esm_datastore":
+                self.check_func(instance, method_name, self._err)
 
 
 class ChainSimplifier(cst.CSTTransformer):
@@ -170,19 +251,36 @@ class ChainSimplifier(cst.CSTTransformer):
     def __init__(self, user_namespace: dict[str, Any] | None = None):
         self.user_namespace = user_namespace or {}
 
-    def leave_Call(self, original_node: cst.Call, updated_node: cst.Call) -> cst.Call:
-        # Use matcher to identify the pattern: any_method(search_call(...))
-        search_pattern = m.Call(
-            func=m.Attribute(value=m.Call(func=m.Attribute(attr=m.Name("search"))))
-        )
+    def leave_Call(
+        self, original_node: cst.Call, updated_node: cst.Call
+    ) -> cst.Call | cst.Name:
+        """
+        Find any chained calls to `search` on an `esm_datastore` object, and
+        replace them with the final result of the search, which is then
+        """
 
-        if m.matches(updated_node, search_pattern):
-            # Extract the method name and inner call
-            inner_call = updated_node.func.value  # type: ignore[attr-defined]
-
-            # Replace the value with the inner call's value to remove the `.search()` call
-            new_func = updated_node.func.with_changes(value=inner_call.func.value)
-            return updated_node.with_changes(func=new_func)
+        match updated_node:
+            case cst.Call(
+                func=cst.Attribute(
+                    value=cst.Name(
+                        value=datastore_obj,
+                    ),
+                    attr=cst.Name(value="search"),
+                )
+            ):
+                instance = self.user_namespace.get(datastore_obj)
+                if type(instance).__name__ != "esm_datastore":
+                    return updated_node
+                search_expr = cst.Module(
+                    [cst.SimpleStatementLine([cst.Expr(original_node)])]
+                ).code.strip()
+                # Evaluate it, put the result back into the user namespace with
+                # the same name as the datastore object - ie. ~ return self
+                self.user_namespace[datastore_obj] = eval(
+                    search_expr, self.user_namespace
+                )
+                # Then update the node to just be the datastore object
+                return cst.Name(value=datastore_obj)
 
         return updated_node
 
@@ -213,11 +311,13 @@ class ChainSimplifier(cst.CSTTransformer):
 
 def load_ipython_extension(ipython: InteractiveShell) -> None:
     """
-    Load the IPython extension and register it to run before cells.
+    Load our IPython extensions
     """
-    # ipython.events.register("pre_run_cell", capture_load_calls)  # type: ignore
     ipython.register_magic_function(
         check_storage_enabled, "cell", "check_storage_enabled"
+    )
+    ipython.register_magic_function(
+        check_dataset_number, "cell", "check_dataset_number"
     )
     return None
 
