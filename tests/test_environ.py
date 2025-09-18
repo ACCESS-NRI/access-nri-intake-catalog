b1fdd4319fbcb64bd6b6936bc207aa8d2097fb7d
Charles Turner <charles.turner@anu.edu.au>
2025-09-18 08:55:32 +0800

    Add test to ensure we are on the fork. ecgtools installation is almost certainly going to break this & I expect CI to fail on this commit.

---
+++ tests/test_environ.py
@@ -0,0 +1,12 @@
+import packaging.version
+
+
+def test_intake_esm_fork():
+    """
+    Make sure intake_esm version > 2025.7.9, ~= we are on our bleeding edge fork
+    """
+    import intake_esm
+
+    assert packaging.version.Version(
+        intake_esm.__version__
+    ) > packaging.version.Version("2025.7.9")
