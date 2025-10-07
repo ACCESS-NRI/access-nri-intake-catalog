2566bd9c5466f3bada1878d4c5e02a4f58ac26fe
Charles Turner <charles.turner@anu.edu.au>
2025-10-07 10:25:31 +0800

    If time bounds are corrupted, catch it and fall back to using time. I haven't minified this dataset - small anyway - so it added an extra dataset to the `test_builder_build` CM3 test

--- tests/test_builders.py
+++ tests/test_builders.py
@@ -29,7 +29,7 @@ from access_nri_intake.source.utils import _NCFileInfo
             7,
         ),
         (["access-esm1-5"], "AccessEsm15Builder", {"ensemble": False}, 11, 11, 10),
-        (["access-cm3"], "AccessCm3Builder", {}, 31, 30, 12),
+        (["access-cm3"], "AccessCm3Builder", {}, 32, 31, 13),
         (["access-om3"], "AccessOm3Builder", {}, 12, 12, 6),
         (["mom6"], "Mom6Builder", {}, 27, 27, 15),
         (["roms"], "ROMSBuilder", {}, 4, 4, 1),
