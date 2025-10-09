468ec4c98fd708b128ff07e76248c1b7ebbb78fa
Charles Turner <52199577+charles-turner-1@users.noreply.github.com>
2025-10-09 15:03:59 +0800

    Build main catalog (ie. `intake.cat.access_nri`) with parquet formatted ESM-Datastores (#518)
    * Main catalog build with parquet, snappy compression - use generated catalogues still csv (usability reasons)
    
    * No longer need to specify `columns_with_iterables` - parquet data format handles that natively
    
    * Update test to verify catalogmanager class is in fact writing `.parquet`, not `.csv`
    
    * add consistent de/re-serialisation test
    
    * Pin pydantic <=2.12

--- tests/test_manager.py
+++ tests/test_manager.py
@@ -2,6 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 
+from pathlib import Path
 from unittest import mock
 from warnings import warn
 
@@ -77,6 +78,10 @@ def test_CatalogManager_build_esm(tmp_path, test_data, builder, basedir, kwargs)
     cat.save()
     cat = CatalogManager(path)
     assert cat.mode == "a"
+
+    # Confirm we wrote out parquet, not csv, files
+    assert not (Path(cat.path).parent / "test.csv").exists()
+    assert (Path(cat.path).parent / "test.parquet").exists()
 
 
 @mock.patch("intake_dataframe_catalog.core.DfFileCatalog.__init__")
